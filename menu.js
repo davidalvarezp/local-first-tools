7afa8dc4fe06744b2e9a043dc3e4256272887d53
davidalvarezp <contact@davidalvarezp.com>
2026-02-02 08:26:33 +0100

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-fetch("menu.html")
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById("menu").innerHTML = html;
-    })
-    .catch(err => console.error("Error cargando el menú:", err));
